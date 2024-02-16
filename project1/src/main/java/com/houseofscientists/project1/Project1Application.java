e7b03ee61ccf66299eef23294deacbca23196747
olia.prosha@gmail.com <olia.prosha@gmail.com>
2024-02-16 17:55:03 +0300

    now

--- project1/src/main/java/com/houseofscientists/project1/Project1Application.java
+++ project1/src/main/java/com/houseofscientists/project1/Project1Application.java
@@ -2,6 +2,8 @@ package com.houseofscientists.project1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Project1Application {
@@ -9,5 +11,10 @@ public class Project1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 	}
+
+
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//	}
 
 }
--- project1/src/main/java/com/houseofscientists/project1/controllers/SomeConroller.java
+++ project1/src/main/java/com/houseofscientists/project1/controllers/SomeConroller.java
@@ -1,12 +1,18 @@
 package com.houseofscientists.project1.controllers;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequestMapping
+@Controller
 public class SomeConroller {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello, this is me";
+    @RequestMapping("/hello")
+    public String sayHello(Model page){
+        page.addAttribute("user", "Dima");
+        return "index";
     }
 }
