77ba88d444bce457723ae3e76ce1cba7aa0a2323
Pronina <olia.prosha@gmail.com>
2024-02-29 14:55:52 +0300

    Olga's push

--- project1/src/main/java/com/houseofscientists/project1/controllers/SomeController.java
+++ project1/src/main/java/com/houseofscientists/project1/controllers/SomeController.java
@@ -1,6 +1,8 @@
-package com.houseofscientists.project1.repository.controllers;
+package com.houseofscientists.project1.controllers;
 
 import com.houseofscientists.project1.models.Questions;
+import com.houseofscientists.project1.repository.QuestionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 @Controller
 public class SomeController {
+    private final QuestionsRepository questionsRepository;
+
+    public SomeController(QuestionsRepository questionsRepository) {
+        this.questionsRepository = questionsRepository;
+    }
+
+
     @RequestMapping("/")
     public String sayHello(Model page){
-        page.addAttribute("user", "TEXT");
+        Iterable <Questions> posts = questionsRepository.findAll();
+        page.addAttribute("prods", posts);
         page.addAttribute("button1", "button");
         return "index";
     }
 
     @RequestMapping("/right_answer")
     public String rightAnswer(Model page){
-        page.addAttribute("user", "TEXT");
-        page.addAttribute("button1", "button");
         return "sliderpage";
     }
 }
